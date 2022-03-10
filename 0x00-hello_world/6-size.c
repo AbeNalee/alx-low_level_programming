4dfce74b9f37dc2fabbf271e3762deadb023a55c
abenalee <abrahamaguvasu@gmail.com>
2022-03-10 14:25:36 -0800

    string

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-	printf("Size of a char: %2d byte(s)", sizeof(char));
-	puts("");
-	printf("Size of an int: %2d byte(s)", sizeof(int));
-	puts("");
-	printf("Size of a long int: %2d byte(s)", sizeof(long int));
-	puts("");
-	printf("Size of a long long int: %2d byte(s)", sizeof(long long int));
-	puts("");
-	printf("Size of a float: %2d byte(s)", sizeof(float));
+	printf("Size of a char: %2d byte(s)"
+			"Size of an int: %2d byte(s)"
+			"Size of a long int: %2d byte(s)"
+			"Size of a long long int: %2d byte(s)"
+			"Size of a float: %2d byte(s)",
+			sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
 
 	return (0);
 }
