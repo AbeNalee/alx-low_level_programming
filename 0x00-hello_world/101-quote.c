3e3254092bd628bc68ad1bd12d513f209e652f83
abenalee <abrahamaguvasu@gmail.com>
2022-03-10 15:02:37 -0800

    split betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr, str);
+	fprintf(stderr, "and that piece of art is useful\"
+		       	- Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
