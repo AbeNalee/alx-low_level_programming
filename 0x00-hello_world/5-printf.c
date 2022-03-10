a26452cccd34a9bb6dfbd67d16008f94b08b975f
abenalee <abrahamaguvasu@gmail.com>
2022-03-10 13:52:32 -0800

    main void

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * main - Function to print out string
  * Return: 0
  */
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
