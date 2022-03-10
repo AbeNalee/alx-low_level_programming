055dfd55a61989479dbaa8b5a8c1fe91b52f763c
abenalee <abrahamaguvasu@gmail.com>
2022-03-10 14:15:58 -0800

    puts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 	printf("Size of a char: %2d byte(s)", sizeof(char));
+	puts("");
 	printf("Size of an int: %2d byte(s)", sizeof(int));
+	puts("");
 	printf("Size of a long int: %2d byte(s)", sizeof(long int));
+	puts("");
 	printf("Size of a long long int: %2d byte(s)", sizeof(long long int));
+	puts("");
 	printf("Size of a float: %2d byte(s)", sizeof(float));
 
 	return (0);
