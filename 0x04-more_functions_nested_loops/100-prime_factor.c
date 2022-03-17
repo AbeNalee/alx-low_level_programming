871efa8e08a0c0dc1914e9885d236f0f02077e8d
Abe <43585616+AbeNalee@users.noreply.github.com>
2022-03-18 00:24:25 +0300

    naming betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-long num = 612852475143;
-long divisor = 2;
-long primeLarge = 0;
+	long num = 612852475143;
+	long divisor = 2;
+	long prime_large = 0;
 
-while (num != 1)
-{
-if (num % divisor == 0)
-{
-num = num / divisor; primeLarge = divisor;
-}
-divisor += 1;
-}
-printf("%ld\n", primeLarge);
+	while (num != 1)
+	{
+		if (num % divisor == 0)
+		{
+			num = num / divisor; prime_large = divisor;
+		}
+		divisor += 1;
+	}
+	printf("%ld\n", prime_large);
 return (0);
 }
